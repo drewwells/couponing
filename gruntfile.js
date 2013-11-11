4a1fdb9440f8091c60189cf84bec23fcaa175459
Drew Wells <drew.wells00@gmail.com>
2013-11-11 09:17:16 -0600

    Proper js/css

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         watch: {
+            options: {
+                livereload: true
+            },
             jade: {
                 files: ['app/views/**'],
                 options: {
@@ -50,7 +53,7 @@ module.exports = function(grunt) {
             }
         },
         concurrent: {
-            tasks: ['nodemon', 'watch'], 
+            tasks: ['nodemon', 'watch'],
             options: {
                 logConcurrentOutput: true
             }
@@ -68,7 +71,7 @@ module.exports = function(grunt) {
         }
     });
 
-    //Load NPM tasks 
+    //Load NPM tasks
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
@@ -80,7 +83,7 @@ module.exports = function(grunt) {
     grunt.option('force', true);
 
     //Default task(s).
-    grunt.registerTask('default', ['jshint', 'concurrent']);
+    grunt.registerTask('default', [/*'jshint',*/ 'concurrent']);
 
     //Test task.
     grunt.registerTask('test', ['env:test', 'mochaTest']);
